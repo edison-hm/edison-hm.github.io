a422c13b97e3c92bbc0b5804b974be467bcf8b76
edison-hm <magichuangmian@sina.com>
2015-12-22 15:55:28 +0800

    nfl

--- NFL/js/index.js
+++ NFL/js/index.js
@@ -1,5 +1,7 @@
 $(function() {
-    $('.swiper-slide1 .logo1, .swiper-slide1 .slogan, .swiper-slide1 .main-img, .loading').addClass('animate-fn');
+    setTimeout(function(){
+        $('.swiper-slide1 .logo1, .swiper-slide1 .slogan, .swiper-slide1 .main-img, .loading').addClass('animate-fn');
+    },1000);
 
     console.log($('.swiper-slide1 .logo'));
     var mySwiper = new Swiper('.swiper-container',{
