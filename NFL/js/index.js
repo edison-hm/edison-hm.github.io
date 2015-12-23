c6c3a8f171c9def5d9989e3190373946a46e05e3
edison-hm <magichuangmian@sina.com>
2015-12-23 10:07:43 +0800

    nfl

--- NFL/js/index.js
+++ NFL/js/index.js
@@ -9,25 +9,27 @@ $(function() {
     $('.swiper-slide1 .slogan').addClass('animate-fn');
     $('.swiper-slide1 .main-img').addClass('animate-fn');
     $('.loading').addClass('animate-fn');
-    // var mySwiper = new Swiper('.swiper-container',{
-    //     speed: 1000,
-    //     onlyExternal : true
-    // })
+
+    var mySwiper = new Swiper('.swiper-container',{
+        speed: 500,
+        onlyExternal : true
+    })
 
     $('.swiper-slide').delegate('.next-btn', 'click', function(event) {
-        nextSwipe()
+        mySwiper.slideNext();
+        // nextSwipe()
     });
 
-    function nextSwipe() {
-        $('.swiper-wrapper')
-            .transition({ transform: "translateX(-"+count*wid+"px)" });
-        // $('.swiper-wrapper').css({
-        //     '-webkit-transform':'translate3D(-'+count*wid+'px,0,0)',
-        //     'transform':'translate3D(-'+count*wid+'px,0,0)',
-        //     'transition': 'all 0.5s'
-        // })
-        count++;
-    }
+    // function nextSwipe() {
+    //     $('.swiper-wrapper')
+    //         .transition({ transform: "translateX(-"+count*wid+"px)" });
+    //     // $('.swiper-wrapper').css({
+    //     //     '-webkit-transform':'translate3D(-'+count*wid+'px,0,0)',
+    //     //     'transform':'translate3D(-'+count*wid+'px,0,0)',
+    //     //     'transition': 'all 0.5s'
+    //     // })
+    //     count++;
+    // }
 
     // page2
     $('.question1-option li').click(function(event) {
@@ -37,8 +39,8 @@ $(function() {
         if(index == 1){
             $('.pop-wrap').fadeIn();
             $('.your-team').click(function() {
-                nextSwipe()
-                // mySwiper.slideNext();
+                // nextSwipe()
+                mySwiper.slideNext();
             })
         }else{
             $(this).addClass('error');
@@ -55,8 +57,8 @@ $(function() {
         if($(this).parent('ul').hasClass('last-answer')){
             console.log(data);
         }else{
-            nextSwipe()
-            // mySwiper.slideNext();
+            // nextSwipe()
+            mySwiper.slideNext();
         }
     })
 
