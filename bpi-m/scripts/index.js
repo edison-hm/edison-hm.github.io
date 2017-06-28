9fcbd56f55f7e0a38311e492ac9353297c6e95f2
edison-hm <magichuangmian@sina.com>
2017-06-28 13:46:32 +0800

    bpi m站目录问题

---
+++ bpi-m/scripts/index.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  // $("#J_sliderWrapper").slide({
+  //   mainCell:"#J_slideBox ul",
+  //   titCell: ".dot-box span",
+  //   effect:"left",
+  //   autoPlay: true,
+  //   pnLoop: true,
+  // });
+
+})
